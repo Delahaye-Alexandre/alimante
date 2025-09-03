357b41ce86e57a3d806a3eb6c6d009a6f796ea9d
Delahaye-Alexandre <alexandre-delahaye@outlook.fr>
2025-09-03 16:50:31 +0200

    Remise à zéro de l'application.

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package principal Alimante
-Système de gestion automatisée des mantes
-"""
-
-__version__ = "1.0.0"
-__author__ = "Alimante Team"
-__description__ = "Système de gestion automatisée des mantes avec Raspberry Pi"
